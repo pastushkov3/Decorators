d8c3b57ae4dfe21b3d18e3ec7e2e0e26bafd4468
Pastushkov <pastuahkov2010@mail.ru>
2022-05-16 18:21:02 +0300

    first commit

---
+++ Decorators.py
@@ -0,0 +1,25 @@
+from functools import wraps
+from datetime import datetime
+import time
+
+def decoration_params(path='log.txt'):
+    def decorations(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            start = time.time()
+            result = func(*args, **kwargs)
+            time_result = time.time() - start
+            log = f'Function name: {func.__name__}\nResult: {result}\nDate: {datetime.now()} Time: {time_result} Args and Kwargs: {args}, {kwargs}\n{"*"*20}\n'
+            with open(path, 'a+', encoding='utf-8') as logs:
+                logs.write(log)
+            return result
+        return wrapper
+    return decorations
+
+@decoration_params()
+def func_summator(a, b):
+    return a + b
+
+if __name__ == '__main__':
+    func_summator(1, 1)
+
